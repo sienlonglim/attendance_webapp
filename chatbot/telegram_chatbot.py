bd7abdde7705249a20918fd2412fded0c6698442
sienlonglim <limsienlong@gmail.com>
2023-04-28 07:50:49 +0800

    added unlink function personal updates still need testing

--- chatbot/telegram_chatbot.py
+++ chatbot/telegram_chatbot.py
@@ -37,8 +37,8 @@ def check_attendance(cohort, urllink):
     soup = BeautifulSoup(page.text, 'html.parser')
 
     # Session code eg. BH92347
-    session = soup.find(class_='alternative-text').find_all('span')
-    session_code = session[3].text.split(': ')[1].split('.')[0]
+    session_page = soup.find(class_='alternative-text').find_all('span')
+    session_code = session_page[3].text.split(': ')[1].split('.')[0]
 
     # API call using the session code, returns a json file containing students signed in
     api_url = f'https://www.myskillsfuture.gov.sg/api/get-attendance?attendanceCode={session_code}&motCode=1'
@@ -67,9 +67,26 @@ def check_attendance(cohort, urllink):
     absent = list(absent)
     present.sort()
     absent.sort()
+
+    currentDateAndTime = datetime.now()
+    currentHour = currentDateAndTime.strftime("%H")
+    currentMin = currentDateAndTime.strftime("%M")
+    hourmin = currentHour + currentMin
+    if int(hourmin) < 1230:
+        session = 'Morning'
+    else:
+        session = 'Afternoon'
     
     # We will pass a dictionary of all the results back to the routing function, which will then be used to render the html
-    return {'session': session_code, 'present':present, 'n_present':len(present), 'absent':absent, 'n_absent':len(absent), 'cohort':cohort[:3].upper()}
+    return {'session_code': session_code,
+            'present':present,
+            'n_present':len(present),
+            'absent':absent,
+            'n_absent':len(absent),
+            'cohort':cohort[:3].upper(),
+            'currentHour':currentHour,
+            'currentMin':currentMin,
+            'session': session}
 
 def get_attendance_links(urllinks):
     '''
@@ -84,45 +101,42 @@ def get_attendance_links(urllinks):
         soup = BeautifulSoup(page.text, 'html.parser')
 
         # Session code eg. BH92347
-        session = soup.find(class_='alternative-text').find_all('span')
-        session_codes.append(session[3].text.split(': ')[1].split('.')[0])
-    return {'session': session_codes}
+        session_page = soup.find(class_='alternative-text').find_all('span')
+        session_codes.append(session_page[3].text.split(': ')[1].split('.')[0])
+    return {'session_code': session_codes}
 
 def build_attendance_message(attendance):
     '''
     Input: dictionary of data from check_attendance function
     Returns the built attendance message
     '''
-    currentDateAndTime = datetime.now()
-    currentHour = currentDateAndTime.strftime("%H")
-    currentMin = currentDateAndTime.strftime("%M")
-    hourmin = currentHour + currentMin
-    if int(currentHour) < 1230:
-        session = 'Morning'
-    else:
-        session = 'Afternoon'
-
-    if len(attendance)>1:
+    if len(attendance)>2:
         # Build absentees output
         if len(attendance['absent'])<1:
             absentees = 'None! =)\n'
         else:
             absentees = ''
             count = 1
-            for x in attendance['absent']:
-                absentees += str(count)+'. ' + x.title() + '\n'
+            for absentee in attendance['absent']:
+                absentees += str(count)+'. ' + absentee.title() + '\n'
                 count +=1
 
         # Build the full message
-        attendance_message = f"*{attendance['cohort']} cohort*\n{session} - {currentHour}{currentMin}hrs:\n{attendance['session']} \n\nTotal present: {attendance['n_present']}\nAbsentees:\n{absentees}"
-    else:
-        attendance_message=f'''*Links for {session} session*:
-JAN - https://www.myskillsfuture.gov.sg/content/portal/en/individual/take-attendance.html?attendanceCode={attendance['session'][0]}&MOT=1#
+        attendance_message = f'''*{attendance['cohort']} cohort*
+{attendance['session']} - {attendance['currentHour']}{attendance['currentMin']}hrs:
+{attendance['session_code']}
 
-FEB - https://www.myskillsfuture.gov.sg/content/portal/en/individual/take-attendance.html?attendanceCode={attendance['session'][1]}&MOT=1#
+Total present: {attendance['n_present']}
+Absentees:
+{absentees}'''
+    else:
+        attendance_message=f'''*Links for {attendance['session']} session*:
+JAN - https://www.myskillsfuture.gov.sg/content/portal/en/individual/take-attendance.html?attendanceCode={attendance['session_code'][0]}&MOT=1#
+
+FEB - https://www.myskillsfuture.gov.sg/content/portal/en/individual/take-attendance.html?attendanceCode={attendance['session_code'][1]}&MOT=1#
         '''
     
-    print(f'Message obtained as follows: \n{"-"*100}\n{attendance_message}\n{"-"*100}')
+    #print(f'Message obtained as follows: \n{"-"*100}\n{attendance_message}\n{"-"*100}')
     return attendance_message
 
 # Start / help command
@@ -136,6 +150,7 @@ def help(message):
                      '1) Get attendance updates for current session press /attendance\n' +
                      '2) Count down to start of OJT press /countdown',
                      reply_markup=keyboard)
+
 
 # Countdown command
 @bot.message_handler(commands=['countdown'])
@@ -159,6 +174,7 @@ def countdown_callback(call):
         bot.send_message(call.message.chat.id, f"{call.data.split()[0]} cohort\n{time_remaining.days} Days remaining until start of OJT ({call.data})")
     bot.answer_callback_query(call.id)
 
+
 # Attendance command
 @bot.message_handler(commands=['attendance'])
 def take_attendance(message):
@@ -180,18 +196,57 @@ def attendance_callback(call):
             attendance = check_attendance(call.data, callback_dict[call.data])
         print('Got attendance, now building message')
         attendance_message = build_attendance_message(attendance)
+        session_active = True
     except Exception as e:
         print(f'Error encountered: {type(e)}{e}')
+        session_active = False
         bot.answer_callback_query(call.id, "No available sessions at the moment.")
     else:      
         bot.send_message(call.message.chat.id, attendance_message, parse_mode='markdown', disable_web_page_preview=True)
-        bot.answer_callback_query(call.id)
+    
+    # 2nd part to send updates to linked accounts - NEED TO INCORPORATE THIS TO HAVE THE LINK AND TIME IF POSSIBLE, also use a list if possible
+    if session_active:
+        try:
+            if len(attendance['absent']) >= 1:
+                cnx = mysql.connector.connect(**config)
+                cursor = cnx.cursor()
+                for absentee in attendance['absent']:
+                    query = '''
+                    SELECT chat_id
+                    FROM students
+                    WHERE student_name = %s and chat_id IS NOT NULL
+                    '''
+                    cursor.execute(query, (absentee,))
+                    personal_message = f'''*{attendance['cohort']} cohort*
+{attendance['session']} - {attendance['currentHour']}{attendance['currentMin']}hrs:
+{attendance['session_code']}
+
+Your attendance is not marked yet
+
+https://www.myskillsfuture.gov.sg/content/portal/en/individual/take-attendance.html?attendanceCode={attendance['session_code']}&MOT=1#
+'''
+                    receiver = cursor.fetchone()[0]
+                    if receiver:
+                        bot.send_message(receiver, personal_message)
+        except Exception as e:
+            print(f'Error encountered: {type(e)}{e}')
+        else:
+            bot.answer_callback_query(call.id)
+        finally:
+            cursor.close()
+            cnx.close()
+
+'''@bot.message_handler(commands=['test'])
+def test(message):
+    attendance = {'absent': ['LIM SIEN LONG']}'''     
+
 
 # Reset accidental keyboard layout modifications
 @bot.message_handler(commands=['reset_keyboard'])
 def reset_keyboard(message):
     remove_markup = telebot.types.ReplyKeyboardRemove()
     bot.send_message(message.chat.id, 'Custom keyboards removed', reply_markup=remove_markup)
+
 
 # Link telegram user to namelist
 @bot.message_handler(chat_types=['private'] , commands=['link'])
@@ -223,9 +278,9 @@ This will allow me to identify and update you personally if you have not taken y
                 bot.send_message(message.chat.id, f'{no_of_matches} matches found - {name_matches}\n\nPlease key in a more specific name for a full match')
             elif no_of_matches == 1:
                 reply_markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-                reply_markup.add(telebot.types.InlineKeyboardButton('Yes', callback_data=f'update_db={name_matches}={message.chat.id}'), 
+                reply_markup.add(telebot.types.InlineKeyboardButton('Yes', callback_data=f'update_db={name_matches[0]}={message.chat.id}'), 
                                 telebot.types.InlineKeyboardButton('No', callback_data=f'update_db=No={message.chat.id}'))
-                bot.send_message(message.chat.id, f'{no_of_matches} match found - {name_matches}\nConfirm update', reply_markup=reply_markup)
+                bot.send_message(message.chat.id, f'{no_of_matches} match found - {name_matches[0]}\nConfirm update', reply_markup=reply_markup)
             else:
                 raise KeyError('0 Match')
         except Exception as err:
@@ -235,21 +290,52 @@ This will allow me to identify and update you personally if you have not taken y
             cursor.close()
             cnx.close()
 
+# Unlink telegram user from namelist
+@bot.message_handler(chat_types=['private'] , commands=['unlink'])
+def unlink_id(message):
+    try:
+        cnx = mysql.connector.connect(**config)
+        cursor = cnx.cursor()
+        query = ('''UPDATE students
+                 SET chat_id = NULL
+                 WHERE chat_id = %s''')
+        cursor.execute(query, (message.chat.id,)) 
+    except Exception as err:
+        print(type(err), err)
+        bot.send_message(message.chat.id, f'Could not find any linked account to this chat')
+    else:
+        cnx.commit()
+        bot.send_message(message.chat.id, 'ChatID unlinked in database')
+    finally:
+        cursor.close()
+        cnx.close()
+
 # Callback query handler for Account linking
 @bot.callback_query_handler(func=lambda call: call.data.startswith('update_db'))
 def link_callback(call):
     data = call.data.split('=')
     if data[1] == 'No':
-        bot.send_message(int(data[2]), 'Request canceled')
+        bot.send_message(call.message.chat.id, 'Request canceled')
     else:
-        bot.send_message(int(data[2]), f'ChatID - {int(data[2])} now tagged to {data[1]}\nThis Chat will receive updates if {data[1]} is absent')
-    bot.answer_callback_query(call.id)
+        try:
+            cnx = mysql.connector.connect(**config)
+            cursor = cnx.cursor()
+            query = ("UPDATE students "
+                     "SET chat_id = %s "
+                     "WHERE student_name = %s")
+            cursor.execute(query, (int(data[2]), data[1]))
+        except Exception as err:
+            print(type(err), err)
+            bot.send_message(call.message.chat.id, f'Oops something went wrong, please check your input and try again.')
+        else:
+            cnx.commit()
+            bot.send_message(call.message.chat.id, f'ChatID - {int(data[2])} now tagged to {data[1]}\nThis Chat will receive updates if {data[1]} is absent')
+        finally:
+            cursor.close()
+            cnx.close()
+            bot.answer_callback_query(call.id)
+        
             
 if __name__ == "__main__":
     print('Bot started')
     bot.infinity_polling()
-
-'''
-@bot.message_handler(func = lambda message : message.chat.type == 'private', commands=['link_telegram_id'])
-def echo(message):
-    bot.reply_to(message, message.text)'''
